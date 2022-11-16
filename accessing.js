f4ec8ba6b4fd982f60c5b5925a091564ed8d1f38
Sarah Dugan <sdoriss@uw.edu>
2022-11-15 21:03:04 -0800

    FirstStart

--- accessing.js
+++ accessing.js
@@ -1,5 +1,16 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
+const headerWeather = document.getElementById('weather-head');
+
+headerWeather.innerText = "February 10 Weather Forecast, Seattle";
+
 
 // Change the styling of every element with class "sun" to set the color to "orange"
 
+const sunClass = document.querySelectorAll('li.sun');
+
+sunClass.forEach((el) => el.style.color = 'orange');
+
+
 // Change the class of the second <li> from to "sun" to "cloudy"
+
+sunClass[1].className = 'cloudy';
--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,33 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
+const lastP = document.getElementsByTagName('p')[0];
+
+const linkBuyNow = document.createElement('a');
+
+const linkText = document.createTextNode('Buy Now!');
+
+linkBuyNow.appendChild(linkText);
+
+linkBuyNow.setAttribute('id', 'cta');
+
+lastP.parentNode.insertBefore(linkBuyNow, lastP.nextSibling)
 
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
 
+const imgCar = document.getElementsByTagName('img')[0];
+
+console.log(imgCar.dataset.color);
+
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
-
+const thirdListItem = document.getElementsByTagName('li')[2];
+thirdListItem.setAttribute('class', 'highlight');
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+const mainBody = document.getElementsByTagName('main')[0];
+
+mainBody.removeChild(lastP);
