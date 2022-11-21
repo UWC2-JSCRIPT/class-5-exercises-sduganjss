1bf5dd12a7c2345323e7912e4d2569608a2a6879
Sarah Dugan <sdoriss@uw.edu>
2022-11-20 16:58:53 -0800

    last changes

--- toDoList.js
+++ toDoList.js
@@ -2,7 +2,6 @@
 const liList = document.getElementsByClassName('today-list')[0];
 const delButton = document.getElementsByClassName('delete')[0];
 
-console.log(liList);
 
 liList.addEventListener('click', e => {
   let liItem = e.target;
@@ -18,11 +17,42 @@ delButton.addEventListener('click', () => {
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
-/*
+const addButton = document.getElementsByClassName('add-item')[0];
+
+addButton.addEventListener('click', (e) => {
+  addListItem(e);
+})
+
 const addListItem = function(e) {
   e.preventDefault();
-  const input = this.parentNode.getElementsByTagName('input')[0];
+ 
+    
+  const input = this.document.getElementsByTagName('input')[0];
   const text = input.value; // use this text to create a new <li>
+//if they hit add and didn't put any text in
+  if (text === '') {
+    return;
+  }
+  //create new elements
+  const liElement = document.createElement('li');
+  const spanElement = document.createElement('span');
+  const linkElement = document.createElement('a');
+  //set their attributes
+  linkElement.setAttribute('class', 'delete');
+  //append them together
+  linkElement.appendChild(document.createTextNode('Delete'));
+  spanElement.appendChild(document.createTextNode(text));
+  //add them to the existing list element
+  liElement.appendChild(spanElement);
+  liElement.appendChild(linkElement);
+ 
+  //now add list element to the ul list
+  liList.appendChild(liElement);
+  //clear out original text
+  input.value  = '';
+  //add listener for new delete button
+  linkElement.addEventListener('click', () => {
+    liList.removeChild(linkElement.parentElement);
+ })
 
-  // Finish function here
-};*/
+};
